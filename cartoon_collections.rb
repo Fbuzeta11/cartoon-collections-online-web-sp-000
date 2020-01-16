973c3bb6d947a6a49aa450efec57da3b1cc26d15
Federico Buzeta Borquez <fbuzeta11@gmail.com>
2020-01-16 00:58:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,6 +11,11 @@ def long_planeteer_calls# code an argument here
 end
 
 def find_the_cheese# code an argument here
-  # the array below is here to help
+array.find do |cheese| 
+    cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
+    end 
+  end 
+  
+find_the_cheese(ingredients)
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
