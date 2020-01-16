da3421fdcdd5f9ef106383d517ac982fe5c1d373
Federico Buzeta Borquez <fbuzeta11@gmail.com>
2020-01-16 00:57:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,14 @@ def summon_captain_planet# code an argument here
 end
 
 def long_planeteer_calls# code an argument here
-  # Your code here
+ for(var i = 0; i < words.length; i++) {
+		if(words[i].length > 4) {
+			return true;
+		}
+	}
+	return false;
+}
+longPlaneteerCalls(["wind", "fire", "earth"]);
 end
 
 def find_the_cheese# code an argument here
